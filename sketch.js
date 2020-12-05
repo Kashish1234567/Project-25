a5acb9219910cdb2734209c3541c966eaa33f9e1
Kashish1234567 <69975135+Kashish1234567@users.noreply.github.com>
2020-12-05 12:00:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,85 +1,57 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-var ground;
-
-
-function preload(){
-	
-
-}
 
 function setup() {
 	createCanvas(1600, 700);
 	engine = Engine.create();
 	world = engine.world;
 
-	ball=Bodies.circle(200,100,20,{restitution:0.3,friction:0.5,density:1.2});
+	ball=Bodies.circle(200,100,20,{restitution:0.1,friction:0.5,density:1.2});
 	ball.image=loadImage("paper.png");
-
 	World.add(world,ball);
 
-   
+    ground = Bodies.rectangle(0, height, 1600, 10 , {isStatic:true} );
+	World.add(world, ground);
+  
   
 
-   dustPos = width/2-100;
-	 dustY = 610;
+   dustPos =1200;
+	 dustY = 640;
 	
-
-	//dustbase=createSprite(boxPos+100,boxY+40,200,20);
-	//dustbase.shapeColor="red";
-
-	dustbasebody = Bodies.rectangle(dustPos+100, dustY+45-20, 200, 20, {isStatic:true});
+	 dustbasebody = Bodies.rectangle(dustPos+100, dustY+45-20, 250, 20, {isStatic:true});
 	World.add(world, dustbasebody);
 
 	dustbasebody.image=loadImage("dustbin.png");
 
-	//dustLeft=createSprite(boxPos+200,boxY-0.1,20,100);
-	//dustLeft.shapeColor="red";
+	boxRightbody = Bodies.rectangle(dustPos+200, dustY+45-20, 20, 210, {isStatic:true});
+	World.add(world,boxRightbody);
 	
-	//dustLeftbody = Bodies.rectangle(boxPos+300, boxY+45-20, 20, 210, {isStatic:true});
-    //World.add(world,boxLeftbody);
-
-	/*boxRight=createSprite(boxPos,boxY-0.1,20,100);
-	boxRight.shapeColor="red";
-	
-	boxRightbody = Bodies.rectangle(boxPos+300, boxY+45-20, 20, 210, {isStatic:true});
-	World.add(world,boxRightbody);*/
-
-
-  ground = Bodies.rectangle(width/2, height, 1600, 10 , {isStatic:true} );
-	World.add(world, ground);
-  
-   
-
-
-
-	
-	//Create the Bodies Here.
-
-
 	Engine.run(engine);
   
 }
-
-
-function draw() {
-  rectMode(CENTER);
+ function draw() {
+  //rectMode(CENTER);
   background(255);
+  imageMode(CENTER);
+  
+  
+
+  //ground.display();
   
   image(ball.image,ball.position.x,ball.position.y,70,70);
-
-  ground.display();
-  
-   
-  image(dustbasebody.image,dustbasebody.position.x,350,200,300);
-     
-
-  drawSprites();
+  image(dustbasebody.image,dustbasebody.position.x,550,250,300);
+  rect(ground.position.x,ground.position.y-5,1600,10);
+  //drawSprites();
  
+}
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(ball,ball.position,{x:90,y:-105});
+  
+	}
 }
 
 
